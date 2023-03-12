7ccbbb70c80c615e62b8974ee5728cb746294c6e
Diego Hernandez <diegohxxiv@gmail.com>
2023-03-11 21:49:11 -0800

    Bug Fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,7 +5,7 @@ export const baseUrl = "https://crypto-update-live.p.rapidapi.com";
 export const fetchApi = async (url: string) => {
   const { data } = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Key": import.meta.env.VITE_API_KEY,
+      "X-RapidAPI-Key": "584966b816msh81d5b95c1ed1131p107bc0jsn02067aa9809f",
       "X-RapidAPI-Host": "crypto-update-live.p.rapidapi.com",
     },
   });
