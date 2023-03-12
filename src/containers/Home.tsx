3417668072eb03fca11e3e18a5b26b607992dc2b
Diego Hernandez <diegohxxiv@gmail.com>
2023-03-12 00:36:50 -0800

    Hero fix

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="w-full h-[85vh] flex flex-col lg:flex-row 2xl:max-w-7xl 2xl:mx-auto">
-      <div className="w-full lg:w-1/2 h-full flex flex-col gap-2 justify-center lg:items-start items-center">
+    <div className="w-full h-[85vh] flex flex-col lg:flex-row 2xl:max-w-7xl 2xl:mx-auto gap-4 lg:gap-0 justify-center items-center">
+      <div className="w-full lg:w-1/2 lg:h-full flex flex-col gap-2 justify-center lg:items-start items-center">
         <p className="font-poppinsBold text-4xl lg:text-5xl text-center lg:text-left">
           Welcome to Bloc
         </p>
@@ -12,7 +12,7 @@ export default function Home() {
           current coin data and trending news.
         </p>
       </div>
-      <div className="w-full lg:w-1/2 h-full grid place-items-center">
+      <div className="w-full lg:w-1/2 lg:h-full grid place-items-center">
         <img
           className="w-1/4 lg:w-[45%] object-cover"
           src="/images/hero.svg"
